62a39a42eca27b4b9c7b6a14ea4f444a149feec1
hungngohihihi <hungngohihihi@gmail.com>
2024-01-11 14:56:50 +0700

    Hung push final function

--- app/importFile/mp3towavSAIIII.py
+++
@@ -1,8 +0,0 @@
-from pydub import AudioSegment
-# sound = AudioSegment.from_mp3("/full/path/filename.mp3")
-# sound.export("/full/path/filename.wav", format="wav")
-sound = AudioSegment.from_mp3("/Users/hung/Downloads/mp3totext-master/input.mp3")
-sound.export("/Users/hung/Downloads/mp3totext-master/output.wav", format="wav")
-
-
-
